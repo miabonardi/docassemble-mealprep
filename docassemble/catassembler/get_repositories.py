0246a4f3cfd95ac773713cff7e1ae1746d352fcf
Laurynas Keturakis <laurynas.keturakis@gmail.com>
2023-11-16 10:46:30 +0100

    filter and cache api response

--- docassemble/catassembler/get_repositories.py
+++ docassemble/catassembler/get_repositories.py
@@ -1,15 +1,32 @@
 from github import Github, Auth
 from docassemble.base.util import get_config
 
+fetched_repos = [] # basic in-memory cache so we don't have to fetch repos every time
+
 def fetch_bellingcat_repos():
     repos = []
+
+    global fetched_repos
+
+    if len(fetched_repos) > 0:
+        return fetched_repos
 
     with GithubClient() as gh:
         for repo in gh.get_organization(login="bellingcat").get_repos():
             if "python" and "command-line" in repo.get_topics():
-                repos.append(repo)
+                data = {
+                    "name": repo.name,
+                    "description": repo.description,
+                    "url": repo.html_url,
+                    "license": repo.get_license().license.name,
+                    "stars": repo.stargazers_count,
+                    "readme": repo.get_readme().decoded_content.decode("utf-8"),
+                    "archive_url": repo.get_archive_link("zipball")
+                }
+                repos.append(data)
 
-    print(repos)
+    fetched_repos = repos
+
     return repos
 
 
